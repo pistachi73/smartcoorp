769779e0c4272ec5d6dcc91d8ce443387d1633d0
pistachi73 <oscarpulido98@gmail.com>
2023-08-04 15:48:59 +0200

    Add portrait image to blog post

--- apps/smart-admin/app/(sidebar)/blog/posts/[postId]/page.tsx
+++ apps/smart-admin/app/(sidebar)/blog/posts/[postId]/page.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { BlogPost } from '@prisma/client';
 import { EditEntryLayout } from '@smart-admin/components/layout';
-import { defaultBlobPostContent, usePostEditor } from '@smart-admin/hooks';
+import {
+  SingleFileUpload,
+  defaultBlobPostContent,
+  usePostEditor,
+  useSingleFileUpload,
+} from '@smart-admin/hooks';
 import { clientTRPC } from '@smart-admin/trpc';
 import { TRPCError } from '@trpc/server';
 import { useEffect, useMemo, useState } from 'react';
@@ -10,9 +16,9 @@ import styled from 'styled-components';
 
 import { useRouter } from 'next/navigation';
 
-import { RouterOutputs } from '@smartcoorp/smart-api';
 import { isNumber } from '@smartcoorp/smart-types';
 import { RHFCheckbox } from '@smartcoorp/ui/checkbox';
+import { RHFFileUpload } from '@smartcoorp/ui/file-upload';
 import { RHFFormField } from '@smartcoorp/ui/form-field';
 import { Col, Grid, Row } from '@smartcoorp/ui/grid';
 import { BlocksDB, PostEditor } from '@smartcoorp/ui/post-editor';
@@ -24,7 +30,12 @@ const FormFieldContainer = styled.div`
   margin-bottom: ${spaceL};
 `;
 
-type PostData = RouterOutputs['blogPost']['getById'];
+type FormData = Omit<
+  BlogPost,
+  'content' | 'portraitImageUrl' | 'createdAt' | 'updatedAt'
+> & {
+  portraitImageUrl?: SingleFileUpload;
+};
 
 type EditPostProps = {
   params: {
@@ -79,42 +90,49 @@ const EditPost = ({ params }: EditPostProps) => {
     [authors]
   );
 
-  const { control, reset, handleSubmit } = useForm<Omit<PostData, 'content'>>({
+  const { control, reset, handleSubmit, setValue } = useForm<FormData>({
     defaultValues: {
       title: '',
       published: false,
       description: '',
       readTime: 0,
+      portraitImageUrl: '',
     },
   });
 
+  const { setCurrentFile: setPortraitImage, handleSingleFileUpload } =
+    useSingleFileUpload({ folder: `blog-post-images/${blogPostId}` });
+
   useEffect(() => {
     reset(data);
+    setPortraitImage(data?.portraitImageUrl);
     setPostBlocks((data?.content as BlocksDB) ?? defaultBlobPostContent);
     setInitialUploadedImages(
       (data?.content as BlocksDB)?.blocks ?? defaultBlobPostContent
     );
-  }, [data, reset, setInitialUploadedImages, setPostBlocks]);
+  }, [data, reset, setInitialUploadedImages, setPortraitImage, setPostBlocks]);
 
-  const onSave = async (data: Omit<PostData, 'content'>) => {
+  const onSave = async (data: FormData) => {
     setIsLoading(true);
     const postBlocks = await handleImages();
+    const portraitImageUrl = await handleSingleFileUpload(
+      data.portraitImageUrl
+    );
+    const postData = { ...data, content: postBlocks, portraitImageUrl };
 
     try {
       if (blogPostId === -1) {
-        const createdPost = await createPost.mutateAsync({
-          ...data,
-          content: postBlocks,
-        });
+        const createdPost = await createPost.mutateAsync(postData);
         router.push(`/blog/posts/${createdPost.id}`);
       } else {
-        await updatePost.mutateAsync({ ...data, content: postBlocks });
+        await updatePost.mutateAsync(postData);
         reset({}, { keepValues: true });
       }
     } catch (e) {
       if (e instanceof TRPCError) console.log(e.message);
     }
 
+    setValue('portraitImageUrl', portraitImageUrl);
     setIsLoading(false);
   };
 
@@ -158,16 +176,32 @@ const EditPost = ({ params }: EditPostProps) => {
                 <Row>
                   <Col size={6}>
                     <Row noMargin>
-                      <Col size={6}>
+                      <Col size={12}>
                         <FormFieldContainer>
                           <RHFFormField
                             control={control}
                             name="title"
                             label="Title"
                             rules={{ required: true }}
+                            isDisabled={isFormLoading}
                           />
                         </FormFieldContainer>
                       </Col>
+                    </Row>
+                    <Row noMargin>
+                      <Col size={6}>
+                        <FormFieldContainer>
+                          <RHFSelect
+                            options={authorOptions}
+                            control={control}
+                            name="authorId"
+                            label="Post Author"
+                            isDisabled={isFormLoading}
+                            rules={{ required: true }}
+                          />
+                        </FormFieldContainer>
+                      </Col>
+
                       <Col size={6}>
                         <FormFieldContainer>
                           <RHFFormField
@@ -175,18 +209,21 @@ const EditPost = ({ params }: EditPostProps) => {
                             name="readTime"
                             label="Reading time (in minutes)"
                             type="number"
+                            isDisabled={isFormLoading}
                             rules={{ required: true }}
                           />
                         </FormFieldContainer>
                       </Col>
                     </Row>
                     <FormFieldContainer>
-                      <RHFSelect
-                        options={authorOptions}
+                      <RHFFormField
+                        height={269}
                         control={control}
-                        name="authorId"
-                        label="Post Author"
+                        name="description"
+                        label="Description"
+                        isDisabled={isFormLoading}
                         rules={{ required: true }}
+                        isMultiline
                       />
                     </FormFieldContainer>
 
@@ -197,13 +234,17 @@ const EditPost = ({ params }: EditPostProps) => {
                     />
                   </Col>
                   <Col size={6}>
-                    <RHFFormField
-                      height={269}
+                    <RHFFileUpload
                       control={control}
-                      name="description"
-                      label="Description"
-                      rules={{ required: true }}
-                      isMultiline
+                      name="portraitImageUrl"
+                      label="Portrait Image"
+                      singleFilePreview={true}
+                      acceptedFileTypes={{
+                        'image/jpeg': [],
+                        'image/jpg': [],
+                        'image/png': [],
+                      }}
+                      isDisabled={isFormLoading}
                     />
                   </Col>
                 </Row>
