1ac3f9d020bb918c861200ce1f8cd9eff1225597
pistachi73 <oscarpulido98@gmail.com>
2023-09-18 15:54:19 +0200

    Testing

--- apps/smart-editor/app/(with-global-nav)/page.tsx
+++ apps/smart-editor/app/(with-global-nav)/page.tsx
@@ -3,9 +3,14 @@ import { Hero } from '@smart-editor/components/landing-page/hero';
 import { Pricing } from '@smart-editor/components/landing-page/pricing';
 import { TryEditor } from '@smart-editor/components/landing-page/try-editor';
 
+import prisma from '@smartcoorp/prisma';
 const Page = async () => {
+  const users = await prisma.user.findMany();
   return (
     <>
+      {users.map((user) => (
+        <div key={user.id}>{user.email}</div>
+      ))}
       <Hero />
       <Features />
       <TryEditor />
