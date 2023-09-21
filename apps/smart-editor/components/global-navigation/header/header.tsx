71fd140f50e7927cff55672d160827aaf55c1935
pistachi73 <oscarpulido98@gmail.com>
2023-09-21 21:04:20 +0200

    remove sesssion from heardere

--- apps/smart-editor/components/global-navigation/header/header.tsx
+++ apps/smart-editor/components/global-navigation/header/header.tsx
@@ -1,6 +1,3 @@
-import { nextAuthConfig } from '@smart-editor/utils/next-auth-config';
-import { getServerSession } from 'next-auth';
-
 import { Button } from '@smartcoorp/ui/button';
 
 import {
@@ -9,30 +6,26 @@ import {
   StyledWidthLimiter,
 } from './header.styles';
 
-export const Header = async () => {
-  const session = await getServerSession(nextAuthConfig);
+export const Header = () => {
   return (
-    <>
-      <HeaderContainer>
-        <StyledWidthLimiter>
-          <Button to="/" variant="text">
-            <img
-              width={80}
-              src={'logo_by_pul_light.svg'}
-              alt="SmartEditor Logo"
-            />
+    <HeaderContainer>
+      <StyledWidthLimiter>
+        <Button to="/" variant="text">
+          <img
+            width={80}
+            src={'logo_by_pul_light.svg'}
+            alt="SmartEditor Logo"
+          />
+        </Button>
+        <AccountButtonsContainer>
+          <Button variant="text" size="small" to="/login">
+            Log in
           </Button>
-          <AccountButtonsContainer>
-            <Button variant="text" size="small" to="/login">
-              Log in
-            </Button>
-            <Button size="small" to="/signup">
-              Get Started
-            </Button>
-          </AccountButtonsContainer>
-        </StyledWidthLimiter>
-      </HeaderContainer>
-      {JSON.stringify(session)}
-    </>
+          <Button size="small" to="/signup">
+            Get Started
+          </Button>
+        </AccountButtonsContainer>
+      </StyledWidthLimiter>
+    </HeaderContainer>
   );
 };
